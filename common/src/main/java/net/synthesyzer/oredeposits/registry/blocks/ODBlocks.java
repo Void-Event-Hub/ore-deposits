05c57996962756432657fdb3a901f7d14998671c
DaanSchar <Daan.Schaer@Gmail.com>
2023-02-02 00:32:36 +0100

    feat: flamnite and voidium deposit emit light

--- common/src/main/java/net/synthesyzer/oredeposits/registry/blocks/ODBlocks.java
+++ common/src/main/java/net/synthesyzer/oredeposits/registry/blocks/ODBlocks.java
@@ -17,19 +17,18 @@ public class ODBlocks {
 
     public static final Supplier<ChargingDepositBlock> CHARGING_DEPOSIT = registerBlock("charging_deposit", true, () -> new ChargingDepositBlock(Block.Properties.copy(Blocks.BEDROCK)));
 
-    public static final Supplier<DepositBlock> COAL_DEPOSIT = registerBlock("coal_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.COAL_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "coal"));
-    public static final Supplier<DepositBlock> COPPER_DEPOSIT = registerBlock("copper_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.COPPER_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "copper"));
-    public static final Supplier<DepositBlock> DIAMOND_DEPOSIT = registerBlock("diamond_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.DIAMOND_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "diamond"));
-    public static final Supplier<DepositBlock> EMERALD_DEPOSIT = registerBlock("emerald_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.EMERALD_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "emerald"));
-    public static final Supplier<DepositBlock> GOLD_DEPOSIT = registerBlock("gold_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.GOLD_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "gold"));
-    public static final Supplier<DepositBlock> IRON_DEPOSIT = registerBlock("iron_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.IRON_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "iron"));
-    public static final Supplier<DepositBlock> LAPIS_DEPOSIT = registerBlock("lapis_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "lapis"));
-    public static final Supplier<DepositBlock> REDSTONE_DEPOSIT = registerBlock("redstone_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "redstone"));
+    public static final Supplier<DepositBlock> COAL_DEPOSIT = registerBlock("coal_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.COAL_ORE).explosionResistance(3600000f).requiresCorrectToolForDrops(), "coal"));
+    public static final Supplier<DepositBlock> COPPER_DEPOSIT = registerBlock("copper_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.COPPER_ORE).explosionResistance(3600000f).requiresCorrectToolForDrops(), "copper"));
+    public static final Supplier<DepositBlock> DIAMOND_DEPOSIT = registerBlock("diamond_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.DIAMOND_ORE).explosionResistance(3600000f).requiresCorrectToolForDrops(), "diamond"));
+    public static final Supplier<DepositBlock> EMERALD_DEPOSIT = registerBlock("emerald_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.EMERALD_ORE).explosionResistance(3600000f).requiresCorrectToolForDrops(), "emerald"));
+    public static final Supplier<DepositBlock> GOLD_DEPOSIT = registerBlock("gold_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.GOLD_ORE).explosionResistance(3600000f).requiresCorrectToolForDrops(), "gold"));
+    public static final Supplier<DepositBlock> IRON_DEPOSIT = registerBlock("iron_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.IRON_ORE).explosionResistance(3600000f).requiresCorrectToolForDrops(), "iron"));
+    public static final Supplier<DepositBlock> LAPIS_DEPOSIT = registerBlock("lapis_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(3600000f).requiresCorrectToolForDrops(), "lapis"));
+    public static final Supplier<DepositBlock> REDSTONE_DEPOSIT = registerBlock("redstone_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(3600000f).requiresCorrectToolForDrops(), "redstone"));
 
     public static final Supplier<DepositBlock> AQUARITE_DEPOSIT = registerBlock("aquarite_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "aquarite"));
-    public static final Supplier<DepositBlock> FLAMNITE_DEPOSIT = registerBlock("flamnite_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "flamnite"));
-    public static final Supplier<DepositBlock> VOIDIUM_DEPOSIT = registerBlock("voidium_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "voidium"));
-
+    public static final Supplier<DepositBlock> FLAMNITE_DEPOSIT = registerBlock("flamnite_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(360000f).requiresCorrectToolForDrops().lightLevel((bs) -> 10), "flamnite"));
+    public static final Supplier<DepositBlock> VOIDIUM_DEPOSIT = registerBlock("voidium_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(360000f).requiresCorrectToolForDrops().lightLevel((bs) -> 10), "voidium"));
 
     @SuppressWarnings("unchecked")
     private static <T extends Block> Supplier<T> registerBlock(String name, boolean createItem, Supplier<T> block) {
