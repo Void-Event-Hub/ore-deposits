e17c19b80eb7639a38370b50bc0f6c6995fd9005
DaanSchar <Daan.Schaer@Gmail.com>
2023-02-01 23:55:22 +0100

    fix: players with full inventory now drop items

--- common/src/main/java/net/synthesyzer/oredeposits/registry/blocks/ODBlocks.java
+++ common/src/main/java/net/synthesyzer/oredeposits/registry/blocks/ODBlocks.java
@@ -17,18 +17,18 @@ public class ODBlocks {
 
     public static final Supplier<ChargingDepositBlock> CHARGING_DEPOSIT = registerBlock("charging_deposit", true, () -> new ChargingDepositBlock(Block.Properties.copy(Blocks.BEDROCK)));
 
-    public static final Supplier<DepositBlock> COAL_DEPOSIT = registerBlock("coal_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.COAL_ORE).explosionResistance(360000f), "coal"));
-    public static final Supplier<DepositBlock> COPPER_DEPOSIT = registerBlock("copper_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.COPPER_ORE).explosionResistance(360000f), "copper"));
-    public static final Supplier<DepositBlock> DIAMOND_DEPOSIT = registerBlock("diamond_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.DIAMOND_ORE).explosionResistance(360000f), "diamond"));
-    public static final Supplier<DepositBlock> EMERALD_DEPOSIT = registerBlock("emerald_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.EMERALD_ORE).explosionResistance(360000f), "emerald"));
-    public static final Supplier<DepositBlock> GOLD_DEPOSIT = registerBlock("gold_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.GOLD_ORE).explosionResistance(360000f), "gold"));
-    public static final Supplier<DepositBlock> IRON_DEPOSIT = registerBlock("iron_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.IRON_ORE).explosionResistance(360000f), "iron"));
-    public static final Supplier<DepositBlock> LAPIS_DEPOSIT = registerBlock("lapis_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(360000f), "lapis"));
-    public static final Supplier<DepositBlock> REDSTONE_DEPOSIT = registerBlock("redstone_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(360000f), "redstone"));
+    public static final Supplier<DepositBlock> COAL_DEPOSIT = registerBlock("coal_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.COAL_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "coal"));
+    public static final Supplier<DepositBlock> COPPER_DEPOSIT = registerBlock("copper_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.COPPER_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "copper"));
+    public static final Supplier<DepositBlock> DIAMOND_DEPOSIT = registerBlock("diamond_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.DIAMOND_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "diamond"));
+    public static final Supplier<DepositBlock> EMERALD_DEPOSIT = registerBlock("emerald_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.EMERALD_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "emerald"));
+    public static final Supplier<DepositBlock> GOLD_DEPOSIT = registerBlock("gold_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.GOLD_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "gold"));
+    public static final Supplier<DepositBlock> IRON_DEPOSIT = registerBlock("iron_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.IRON_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "iron"));
+    public static final Supplier<DepositBlock> LAPIS_DEPOSIT = registerBlock("lapis_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "lapis"));
+    public static final Supplier<DepositBlock> REDSTONE_DEPOSIT = registerBlock("redstone_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "redstone"));
 
-    public static final Supplier<DepositBlock> AQUARITE_DEPOSIT = registerBlock("aquarite_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(360000f), "aquarite"));
-    public static final Supplier<DepositBlock> FLAMNITE_DEPOSIT = registerBlock("flamnite_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(360000f), "flamnite"));
-    public static final Supplier<DepositBlock> VOIDIUM_DEPOSIT = registerBlock("voidium_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(360000f), "voidium"));
+    public static final Supplier<DepositBlock> AQUARITE_DEPOSIT = registerBlock("aquarite_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "aquarite"));
+    public static final Supplier<DepositBlock> FLAMNITE_DEPOSIT = registerBlock("flamnite_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "flamnite"));
+    public static final Supplier<DepositBlock> VOIDIUM_DEPOSIT = registerBlock("voidium_deposit", true, () -> new DepositBlock(Block.Properties.copy(Blocks.LAPIS_ORE).explosionResistance(360000f).requiresCorrectToolForDrops(), "voidium"));
 
 
     @SuppressWarnings("unchecked")
--- common/src/main/java/net/synthesyzer/oredeposits/registry/events/DepositBreakEventHandler.java
+++ common/src/main/java/net/synthesyzer/oredeposits/registry/events/DepositBreakEventHandler.java
@@ -13,9 +13,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootContext;
-import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.LootTables;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.synthesyzer.oredeposits.registry.blocks.ODBlocks;
 import net.synthesyzer.oredeposits.registry.blocks.custom.block.ChargingDepositBlock;
@@ -46,9 +43,9 @@ public class DepositBreakEventHandler {
         }
 
         if (block instanceof DepositBlock depositBlock) {
-            dropLootTable((ServerLevel) level, player, blockState);
+            givePlayerBlockLootTable((ServerLevel) level, player, blockState);
+            level.playSound(null, player.blockPosition(), SoundEvents.ITEM_PICKUP, player.getSoundSource(), 0.6f, 1f + (float) Math.random() * 0.5f);
             damageMainHandTool(player);
-
             brokenBlocks.put(blockPos, depositBlock);
 
             level.setBlock(blockPos, ODBlocks.CHARGING_DEPOSIT.get().defaultBlockState(), 3);
@@ -62,25 +59,27 @@ public class DepositBreakEventHandler {
         player.getMainHandItem().hurtAndBreak(1, player, (p) -> p.broadcastBreakEvent(player.getUsedItemHand()));
     }
 
-    private static void dropLootTable(ServerLevel world, Player player, BlockState state) {
-        LootTables lootManager = world.getServer().getLootTables();
-        LootTable lootTable = lootManager.get(state.getBlock().getLootTable());
-        LootContext.Builder builder = new LootContext.Builder(world)
+    private static void givePlayerBlockLootTable(ServerLevel level, Player player, BlockState blockState) {
+        List<ItemStack> lootTable = getLootTable(level, player, blockState);
+
+        for (ItemStack itemStack : lootTable) {
+            if (player.getInventory().getFreeSlot() == -1) {
+                player.drop(itemStack, false);
+            } else {
+                player.addItem(itemStack);
+            }
+        }
+
+        player.giveExperiencePoints(1);
+    }
+
+    private static List<ItemStack> getLootTable(ServerLevel world, Player player, BlockState state) {
+        return state.getDrops(new LootContext.Builder(world)
                 .withLuck(player.getLuck())
                 .withRandom(player.getRandom())
                 .withParameter(LootContextParams.ORIGIN, player.position())
                 .withParameter(LootContextParams.TOOL, player.getMainHandItem())
                 .withParameter(LootContextParams.THIS_ENTITY, player)
-                .withParameter(LootContextParams.BLOCK_STATE, state);
-
-        world.playSound(null, player.blockPosition(), SoundEvents.ITEM_PICKUP, player.getSoundSource(), 0.6f, 1f + (float) Math.random() * 0.5f);
-
-        player.giveExperiencePoints(1);
-
-        List<ItemStack> lootTables = lootTable.getRandomItems(builder.create(LootContextParamSets.BLOCK));
-
-        for (ItemStack lootTableEntry : lootTables) {
-            player.addItem(lootTableEntry);
-        }
+                .withParameter(LootContextParams.BLOCK_STATE, state));
     }
 }
